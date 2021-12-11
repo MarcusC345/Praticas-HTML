24aadee76b8f34615613bc83b2aa7159b625a63d
MarcusC345 <91568314+MarcusC345@users.noreply.github.com>
2021-12-10 22:08:17 -0300

    Delete Script1.js

--- Script1.js
+++
@@ -1,9 +0,0 @@
-function somarValores(){
-  const firstNumber = Number(document.getElementById("first_number").value);
-  const secondNumber = Number(document.getElementById("second_number").value);
-  const soma = firstNumber + secondNumber;
-  
-  console.log(soma)
-  document.getElementById("resultado").innerHTML = soma;
-
-}
